311b9e5340425eb0fc2044e5c83c08c100320ba9
Hassankhaleel <hk450408@gmail.com>
2025-06-21 20:03:50 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>
+        lets build NextDevNATION
+      </h1>
+      <p>
+
+        AB KAAM KRNA HA BABO BHAIAA @@
+
+      </p>
+    </>
+
+
+  )
+}
+
+export default App
